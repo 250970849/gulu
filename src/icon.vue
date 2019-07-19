db314ec5099ea99e8309eb7d8f961b73421e78a2
lihaoyu <18086414256@sina.cn>
2019-07-19 18:25:24 +0800

    将 SVG 代码整理到 icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,13 @@
+<templat>
+    <svg class="g-icon">
+        <use :xlink:href="'#i-${name}'"></use>
+    </svg>
+</templat>
+<script>
+export  default {
+    props: ['name']
+}
+</script>
+<style>
+    .icon{width: 1em; height: 1em;}
+</style>
